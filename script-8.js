34186dd9d9b15be1f79c6d5afb5f58b9b0a1076a
MonishKumarRMK <125937627+MonishKumarRMK@users.noreply.github.com>
2023-04-15 17:05:16 +0530

    Add files via upload

---
+++ script-8.js
@@ -0,0 +1,96 @@
+function labelcreate(tagname,attrname,attrvalue,content){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.innerHTML=content;
+    return ele;
+}
+
+function linebreakers(tagname){
+    var ele=document.createElement(tagname);
+    return ele;
+}
+function inputele(tagname,attrname,attrvalue,attrname1,attrvalue1){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.setAttribute(attrname1,attrvalue1);
+    return ele;
+
+    
+
+//     <table class="table">
+//   <thead>
+//     <tr>
+//       <th scope="col">First</th>
+//       <th scope="col">Last</th>
+//       <th scope="col">Handle</th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     <tr>
+//       <td>Mark</td>
+//       <td>Otto</td>
+//       <td>@mdo</td>
+//     </tr>
+//   </tbody>
+// </table>
+// }
+
+
+
+
+// var firstname=labelcreate("label","for","firstname","First Name");
+// var br1=linebreakers("br");
+// var firstnameinput=inputele("input","type","firstname","id","firstname");
+// var br2=linebreakers("br"); 
+// document.body.append(firstname,br1,firstnameinput,br2);
+
+// var middlename=labelcreate("label","for","middlename","Middle Name");
+// var br1=linebreakers("br");
+// var middlenameinput=inputele("input","type","middlename","id","middlename");
+// var br2=linebreakers("br"); 
+// document.body.append(middlename,br1,middlenameinput,br2);
+
+// var lastname=labelcreate("label","for","middlename","Last Name");
+// var br1=linebreakers("br");
+// var lastnameinput=inputele("input","type","middlename","id","last name");
+// var br2=linebreakers("br"); 
+// document.body.append(lastname,br1,lastnameinput,br2);
+
+// var email=labelcreate("label","for","middlename","Email");
+// var br1=linebreakers("br");
+// var emailinput=inputele("input","type","email","id","email");
+// var br2=linebreakers("br"); 
+// document.body.append(email,br1,emailinput,br2);
+
+// var password=labelcreate("label","for","password","Password");
+// var br1=linebreakers("br");
+// var passwordinput=inputele("input","type","email","id","password");
+// var br2=linebreakers("br"); 
+// document.body.append(password,br1,passwordinput,br2);
+
+
+
+
+// var middlename=labelcreate("label","for","middle name","middle name");
+// var br=linebreakers("br");
+// var inputmiddlename=inputcreate("input","type","middle name","id","middle name");
+// var br1=linebreakers("br1"); 
+// document.body.append(middlename,br,inputmiddlename,br1);
+
+// var lastname=labelcreate("label","for","last name","last name");
+// var br=linebreakers("br");
+// var inputlastname=inputcreate("input","type","last name","id","last name");
+// var br1=linebreakers("br1"); 
+// document.body.append(lastname,br,inputlastname,br1);
+
+// var email=labelcreate("label","for","email","email");
+// var br=linebreakers("br");
+// var inputemail=inputcreate("input","type","mail","id","email");
+// var br1=linebreakers("br1"); 
+// document.body.append(email,br,inputemail,br1);
+
+// var password=labelcreate("label","for","password","password");
+// var br=linebreakers("br");
+// var inputpassword=inputcreate("input","type","password","id","password");
+// var br1=linebreakers("br1"); 
+// document.body.append(password,br,inputpassword,br1);
